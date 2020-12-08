e1df6544ff6e2f407cf56b625c2a206e6cf4002a
ZeldaZach <zahalpern+github@gmail.com>
2020-12-08 00:30:57 -0500

    Small fixup for Day 8

--- entries/day8/main.py
+++ entries/day8/main.py
@@ -1,31 +1,29 @@
-import operator
 import pathlib
 from typing import List, Tuple
 import re
 
 
 def day8_part1(data: List[str]) -> Tuple[int, bool]:
-    operators = {"+": operator.add, "-": operator.sub}
-    instruction_regex = re.compile(r"(.*) (\+|-)(\d+)")
+    instruction_regex = re.compile(r"(.*) ([+\-]\d+)")
 
     visited_nodes = set()
     accumulator = 0
     instruction_index = 0
-    while instruction_index < len(data):
+    while 0 <= instruction_index < len(data):
         if instruction_index in visited_nodes:
             return accumulator, False
         visited_nodes.add(instruction_index)
 
-        instruction, op, value = instruction_regex.findall(data[instruction_index])[0]
+        instruction, value = instruction_regex.findall(data[instruction_index])[0]
         if instruction == "nop":
             instruction_index += 1
         elif instruction == "acc":
-            accumulator = operators[op](accumulator, int(value))
+            accumulator += int(value)
             instruction_index += 1
         elif instruction == "jmp":
-            instruction_index = operators[op](instruction_index, int(value))
+            instruction_index += int(value)
 
-    return accumulator, True
+    return accumulator, instruction_index == len(data)
 
 
 def day8_part2(data: List[str]) -> int:
@@ -55,5 +53,5 @@ def get_input_data(file: str) -> List[str]:
 
 
 if __name__ == "__main__":
-    print(day8_part1(get_input_data("input.txt")))
+    print(day8_part1(get_input_data("input.txt"))[0])
     print(day8_part2(get_input_data("input.txt")))
